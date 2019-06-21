8b15d581879d39634104eadb331e154bc4bf827c
amiani <amianijohns@gmail.com>
2019-06-21 19:09:08 -0400

    socket connection working with ack

--- .owc-dev-server.config.js
+++ .owc-dev-server.config.js
@@ -1,9 +1,18 @@
+const store = require('./store')
+
 module.exports = function(app) {
   const http = require('http').createServer(app)
   const io = require('socket.io')(http)
 
   io.on('connection', function(socket) {
-    console.log('hello')
+    console.log('player client connected')
+    socket.on('player-join', (data, cb) => {
+      if (data) {
+        console.log('player joined')
+        store[data] = { name: data }
+        cb(true)
+      }
+    })
   })
 
   http.listen(8081, function() {
--- src/player/player-app.js
+++ src/player/player-app.js
@@ -3,6 +3,7 @@ import O from 'patchinko/constant.mjs'
 
 import PlayerLobby from './player-lobby'
 
+const socket = io('http://localhost:8081')
 const store = {
 	Initial: () => ({
 		name: '',
@@ -10,14 +11,17 @@ const store = {
 	
 	Actions: update => ({
 		submitName: name => {
-			update({ name })
+			socket.emit('player-join', name, data => {
+				if (data === true)
+					update({ name })
+			})
 		}
 	})
 }
 
 const update = flyd.stream()
 const states = flyd.scan(O, store.Initial(), update)
-const actions = store.Actions(update)
+const actions = store.Actions(update, socket)
 
 customElements.define('player-app', class PlayerApp extends LitElement {
 	static get properties() {
@@ -31,7 +35,6 @@ customElements.define('player-app', class PlayerApp extends LitElement {
 	}
 
 	firstUpdated() {
-		this.socket = io('http://localhost:8081')
 		states.map(state => this.state = { ...state })
 	}
 
