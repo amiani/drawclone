535c50e9a8448bc54ce27a57aefa0033137d6d71
amiani <amianijohns@gmail.com>
2019-06-23 11:00:04 -0400

    log player name when joining

--- .owc-dev-server.config.js
+++ .owc-dev-server.config.js
@@ -1,21 +1,21 @@
-const store = require('./store')
+const store = require('./store');
 
 module.exports = function(app) {
-  const http = require('http').createServer(app)
-  const io = require('socket.io')(http)
+  const http = require('http').createServer(app);
+  const io = require('socket.io')(http);
 
   io.on('connection', function(socket) {
-    console.log('player client connected')
+    console.log('player client connected');
     socket.on('player-join', (data, cb) => {
       if (data) {
-        console.log('player joined')
-        store[data] = { name: data }
-        cb(true)
+        console.log(`${data} joined`);
+        store[data] = { name: data };
+        cb(true);
       }
-    })
-  })
+    });
+  });
 
   http.listen(8081, function() {
-    console.log('socket listening on 8081')
-  })
-}
+    console.log('socket listening on 8081');
+  });
+};
