9ac43e39bd01715e469785b8d18cf9adeba1fbaa
amiani <amianijohns@gmail.com>
2019-07-02 16:06:08 -0400

    updating input width with window width

--- src/player/player-titling.js
+++ src/player/player-titling.js
@@ -15,6 +15,19 @@ export default customElements.define('player-titling', class PlayerTitling exten
     this.title = ''
   }
 
+  firstUpdated() {
+    this.wiredInputWidth = this.shadowRoot.querySelector('wired-input').getBoundingClientRect().width
+    window.addEventListener('resize', this.resetInputWidth)
+  }
+
+  disconnectedCallback() {
+    window.removeEventListener('resize', this.resetInputWidth)
+  }
+
+  resetInputWidth(e) {
+    this.wiredInputWidth = this.shadowRoot.querySelector('wired-input').getBoundingClientRect().width
+  }
+
   handleKeydown(e) {
     if (e.key === 'Enter')
       this.actions.submitTitle(this.title)
@@ -25,6 +38,11 @@ export default customElements.define('player-titling', class PlayerTitling exten
       :host {
         display: flex;
         justify-content: center;
+        min-width: 50%;
+      }
+
+      wired-input {
+        flex-grow: 1;
       }
 
       p {
@@ -38,14 +56,13 @@ export default customElements.define('player-titling', class PlayerTitling exten
 
     return this.state.isTitleSubmitted ?
       html`<p>Wait for everyone else</p>` :
-      html`<div>
+      html`
         <wired-input
           type='text'
           placeholder='What is that...?'
           .value=${this.title}
           @keydown=${this.handleKeyDown}
           @change=${e => this.title = e.target.value}></wired-input>
-        <wired-button @click=${e=>this.actions.submitTitle(this.title)}>Submit</wired-button>
-      </div>`
+        <wired-button @click=${e=>this.actions.submitTitle(this.title)}>Submit</wired-button>`
   }
 })
