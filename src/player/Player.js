7b6c565a4d1ac84ab7ef04cfd35176a1ca1a45e2
amiani <amianijohns@gmail.com>
2019-07-05 22:17:54 -0400

    added in-scheme colors

--- src/player/Player.js
+++ src/player/Player.js
@@ -1,4 +1,4 @@
-const colors = ['green', 'red', 'black', 'white', 'pink', 'purple']
+const colors = ['#31263e', '#ecebe4', '#80ff72', '#eec0c6', '#e63946', '#565554', '#97cc04', ]
 let colorIndex = 0
 module.exports = class Player {
   constructor(socket, name, isLeader, getPhase) {
